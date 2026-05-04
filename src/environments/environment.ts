15badcb8bafbaa021d3f1d1f1f8455925947f686
Gabriel Juarez de la Cruz <gjuarez@agoconsultores.com.mx>
2026-05-04 11:10:37 -0600

    chore: update production api url and prepare for deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8084/api/v1'
+  apiUrl: 'https://dental-api-system-473791225844.northamerica-south1.run.app/api/v1'
 };
