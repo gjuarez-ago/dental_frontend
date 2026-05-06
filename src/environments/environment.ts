bb42975300ef040f18efa4264769fcfd77a15bfc
Gabriel Juarez de la Cruz <gjuarez@agoconsultores.com.mx>
2026-05-06 16:24:14 -0600

    Fix: seguridad de login (autocomplete off), headers de seguridad y actualización de API URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8084/api/v1',
+  apiUrl: 'https://dental-api-system-473791225844.northamerica-south1.run.app/api/v1',
   mapboxToken: 'pk.eyJ1IjoiZ2FqdWFyZXpkZWxhYyIsImEiOiJjbW1qb2FrZzYxM29mMnFwendibHl6bDU1In0.oqb9SV0cCc0iyosOAoQ38g'
 };
